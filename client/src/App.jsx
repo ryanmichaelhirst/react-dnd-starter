8cd6af71cba4fb744896acdae98a5d6bedeac982
RyanMichael Hirst <rmbh4211995@gmail.com>
2020-02-09 17:40:49 -0500

    Init repo

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Homepage from "./pages/Homepage";
+
+const App = () => {
+    return (
+        <Homepage />
+    );
+};
+
+export default App;
---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = props => {
+    return (
+        <div>
+            Time to start coding!
+        </div>
+    );
+};
+
+export default Homepage;
